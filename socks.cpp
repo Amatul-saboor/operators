f544171bbfbc8f9419cc311abccaded93ca53adb
Amatul-saboor <137865822+Amatul-saboor@users.noreply.github.com>
2023-07-03 13:43:12 -0400

    Update socks.cpp

--- socks.cpp
+++ socks.cpp
@@ -1,5 +1,8 @@
+//PrepBuddy is getting late for college. She is looking for a matching pair of socks from a box full of socks.
+In the worst-case scenario, how many socks should PrepBuddy remove from her drawer until she finds a matching pair.
+
 #include <iostream>
-  using namespace std;
+using namespace std;
   
   int main()
   {
