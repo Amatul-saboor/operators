db93894194c831186c72ced256585be9b8e2f998
Amatul-saboor <137865822+Amatul-saboor@users.noreply.github.com>
2023-07-03 13:41:44 -0400

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -1,6 +1,7 @@
+//Write a program to take two integers x and 
+y as input from the user, multiply those integers and print the product.
 #include <iostream>
-  using namespace std;
-  
+using namespace std;
   int main()
   {
     int x,y;
