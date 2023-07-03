f28d7c1580399aebbe7f424320d282e727fb589f
Amatul-saboor <137865822+Amatul-saboor@users.noreply.github.com>
2023-07-03 13:37:35 -0400

    Update Analogclock.cpp

--- clock.cpp
+++ clock.cpp
@@ -1,3 +1,6 @@
+//analog clock which consists of two hands one for hour and another for minute.
+She/he wants to calculate the shorter angle formed between hour and minute hand at any given time.
+//CODE
 #include<iostream>
 using namespace std;
 
